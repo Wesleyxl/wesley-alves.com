7887167f2ea824db937cef790bc495c87a5cabed
Wesleyxl <wesley.alvesxll@gmail.com>
2022-03-14 10:48:49 -0300

    structure done

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #00022E;
+    color: #ffffff;
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+  }
+
+`;
